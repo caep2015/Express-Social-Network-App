82cc2c713e0c6e3b96a36933d4205237f481a0c0
Carlota Pearl <carlota.pearl@gmail.com>
2017-08-13 19:17:38 -0400

    further setup

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mustacheExpress = require('mustacheExpress');
+const bodyParser = require('body-parser');
+const validator = require('express-validator');
+const session = require('express-session');
+const controllers = require('./controllers/controllers.js');
+const parseurl = require('parseurl');
+const routes = require('./router');
+const path = require('path');
+
+const app = express();
+
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+routes(app);
+
+app.listen(3000, function(){
+  console.log('Let the gabblin begin...')
+});
---
+++ controllers/controllers.js
@@ -0,0 +1,14 @@
+module.exports = {
+  home: function(req, res){
+    res.redirect('/signup');
+  },
+
+  signup: fuction(req, res) {
+    res.send('SIGNUP')
+  }
+
+
+
+
+  
+};
